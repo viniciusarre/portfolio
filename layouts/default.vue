ba2ff2fdb63da809265136993c04b139d15dd408
Vinícius <viniciusarre@gmail.com>
2020-09-15 07:06:24 -0300

    Add legacy version in nuxt.js

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@
     <v-main>
       <v-container fill-height fluid>
         <v-row align="center" justify="center">
-          <slot />
+          <nuxt />
         </v-row>
       </v-container>
     </v-main>
@@ -18,8 +18,8 @@
 </template>
 
 <script>
-import Navbar from '../components/NavBar'
-import MobileNavbar from '../components/MobileNavBar'
+import Navbar from '@/components/NavBar'
+import MobileNavbar from '@/components/MobileNavBar'
 export default {
   components: { Navbar, MobileNavbar },
   data () {
