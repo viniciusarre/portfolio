df1fe45dff6ef92a20941babee0ecfad27d67a30
Vinícius <viniciusarre@gmail.com>
2020-12-25 22:27:57 -0300

    remove .nuxt folder

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as MobileNavBar } from '../../components/MobileNavBar.vue'
-export { default as NavBar } from '../../components/NavBar.vue'
-
-export const LazyMobileNavBar = import('../../components/MobileNavBar.vue' /* webpackChunkName: "components/MobileNavBar'}" */).then(c => c.default || c)
-export const LazyNavBar = import('../../components/NavBar.vue' /* webpackChunkName: "components/NavBar'}" */).then(c => c.default || c)
--- .nuxt/vuetify/options.js
+++
@@ -1,1 +0,0 @@
-export default {"theme":{"dark":true,"themes":{"dark":{"primary":"#8BC34A","accent":"#455A64","secondary":"#757575","info":"#FFFFFF","success":"#8BC34A"}}}}
